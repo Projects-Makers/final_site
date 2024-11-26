1bfe5e2f51f680e659fcb7411bcf24908d250be6
Oskar Jabłoński <jablonskio@tlimc.local>
2024-11-26 11:40:04 +0100

    e

--- php/login/action_php/login_action.php
+++ php/login/action_php/login_action.php
@@ -29,7 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if (password_verify($password, $hashed_password)) {
             $_SESSION['user_id'] = $id;
             $_SESSION['username'] = $username;
-      
+            echo "Jesteś zalogowany";
         } else {
             echo "Nieprawidłowe hasło.";
         }
@@ -41,5 +41,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 $conn->close();
 ?>
-<center>Jestes zalogowany,Witaj<center>
 </section>
