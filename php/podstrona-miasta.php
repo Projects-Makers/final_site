470adda06a092eac75e2ef2dc4fd6969ca5d4ebb
gnatjaku <jakub.gnat@gmail.com>
2024-11-27 09:40:42 +0100

    dsfds
    dsfdsfdsf

--- php/podstrona-miasta.php
+++ php/podstrona-miasta.php
@@ -11,7 +11,13 @@
     ?>
 </div>
 <hr style="width: 450px; height: 1px; background-color: gray; "></hr>
-<div class="info1">Informacje</div>
+<div class="info1">
+    <?php
+    $wynik = mysqli_query($conn,"SELECT * FROM  miasta where id='$nr'");
+    $wiersz = mysqli_fetch_array($wynik);
+    echo '<b>' . $wiersz["opis_miasto"] . '</b>';
+    ?>
+</div>
 <hr style="width: 450px; height: 1px; background-color: gray; "></hr>
 <div class="czcionka"><b>Atrakcje</b></div>
 <hr style="width: 450px; height: 1px; background-color: gray; "></hr>
