3f585d34f02f39339122048a7a147d6f16ad79b4
Umbro9 <lolxdlolfabek@gmail.com>
2024-11-28 15:17:43 +0100

    cos dziala

--- index.php
+++ index.php
@@ -12,6 +12,9 @@
     <link rel="stylesheet" href="style/light-darck.css">
     <link rel="stylesheet" href="style/strona.css">
     <link rel="stylesheet" href="style/podstrona.css">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+     crossorigin=""/>
 </head>
 <body>      
 <?php 
@@ -37,3 +40,6 @@ include("php/footer.php");
 
 <script src="javascript/login.js"></script>
 <script src="javascript/register.js"></script>
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+     crossorigin=""></script>
--- php/section.php
+++ php/section.php
@@ -7,7 +7,7 @@ include("php/sidebar.php");
    
     <img src="pic/mapa.png" width="700px" height="800px" id="clickable-image">
  
-    <div id="red-object" class="hidden"></div>
+    <div id="red-object" class="hidden"><div id="map" style="width: 1100px; height: 720px;"></div><div id="close-button">Close</div></div>
     <script src="javascript/mapa.js"></script>
     </div>
 </div>
