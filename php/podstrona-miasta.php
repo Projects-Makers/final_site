525fc52bdc41ee9a155506d08202ff1ef4e97994
gnatjaku <jakub.gnat@gmail.com>
2024-12-03 11:24:14 +0100

    e
    e

--- php/podstrona-miasta.php
+++ php/podstrona-miasta.php
@@ -26,17 +26,19 @@ Informacje
 <hr style="width: 450px; height: 1px; background-color: gray; "></hr>
 <?php
 require("config.php"); 
-$wynik = mysqli_query($conn,"SELECT * FROM atrakcje");
+$nr = $_GET["miasto"]; // Pobranie ID miasta z URL
+$wynik = mysqli_query($conn,"SELECT * FROM atrakcje WHERE miasto='$nr'"); // Filtrowanie atrakcji dla danego miasta
 while ($wiersz = mysqli_fetch_array($wynik))
 {
    echo '<div class="info2">'; // Dodanie stylizowanego bloku
-   echo '<div class="czcionka">' . $wiersz["nazwa"] . '</div>';
-   echo '<p> Cena: ' . $wiersz["cena"] . '</p>';
-   echo '<p> Godziny otwarcia: ' . $wiersz["godz"] . '</p>';
-   echo '<p> Lokalizacja: ' . $wiersz["lokalizacja"] . '</p>';
+   echo '<div class="czcionka">' . htmlspecialchars($wiersz["nazwa"]) . '</div>';
+   echo '<p> Cena: ' . htmlspecialchars($wiersz["cena"]) . '</p>';
+   echo '<p> Godziny otwarcia: ' . htmlspecialchars($wiersz["godz"]) . '</p>';
+   echo '<p> Lokalizacja: ' . htmlspecialchars($wiersz["lokalizacja"]) . '</p>';
    echo '</div>'; // Zakończenie bloku
 }
 ?>
+
 </div>
 </div>
 </div>
