d163594d17cd6142b9a1026d6fb77e20cc031750
Umbro9 <lolxdlolfabek@gmail.com>
2025-01-10 17:40:48 +0100

    h

--- php/sidebar.php
+++ php/sidebar.php
@@ -7,7 +7,7 @@
 
     <div class="extra">
         <a href="index.php?strona=srodek"><div class="extra-icon"><img src="icons/home.png" alt="Home">Głowna</div></a>
-        <div class="extra-icon"><img src="icons/map.png" alt="Map">Mapa</div>
+        <div class="extra-icon mapa-button"><img src="icons/map.png" alt="Map">Mapa</div>
         <a href="#"><div class="extra-icon"><img src="icons/shop.png" alt="Map">Sklep</div></a>
         <div class="extra-icon konto"><img src="icons/user.png" alt="Map">Konto</div>
         <a href="#"><div class="extra-icon"><img src="icons/cogwheel.png" alt="Map">Ustawienia</div></a>
