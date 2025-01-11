ec0aab40efe51f97e81db7967c816f95c1859a57
gniot2137 <gnat.filip@gmail.com>
2025-01-11 17:35:32 +0100

    s
    s

--- php/informacje.php
+++ php/informacje.php
@@ -26,5 +26,27 @@ while ($wiersz = mysqli_fetch_array($wynik)){
 }
 ?>
 </div>
+<hr style="width: 30vh; border: 0.2vh solid black;">
+<div class="czcionka1">Restauracje</div>
+<hr style="width: 30vh; border: 0.2vh solid black;">
+<?php
+require("config.php");
+
+$nr = $_GET["nr"];
+
+$wynik = mysqli_query($conn,"SELECT * from restauracje where lp = $nr");
+while ($wiersz = mysqli_fetch_array($wynik)){
+    
+    $imagePath = '../zdj/' . $wiersz["id_restauracje"] . '.jpg';
+    echo '<div class="city-card">';
+    if (file_exists($imagePath)) {
+        echo '<a href="index.php?strona=#&nr=' . $wiersz["id"] . '"><img src="' . $imagePath . '></a>';
+    } else {
+        echo '<img src="zdj/nic.png">';
+    }
+    echo '' . $wiersz["nazwa"] . '';
+    echo '</div>';
+}
+?>
 </center>
 </section>
