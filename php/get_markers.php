83ae1a2a83bf3827a82a3730d94425d7a65f93b8
Okruch165 <oskarjablonski272@gmail.com>
2025-01-11 18:57:36 +0100

    s

--- php/get_markers.php
+++ php/get_markers.php
@@ -7,21 +7,14 @@ $username = 'root';
 $password = '';
 
 try {
-    // Połączenie z bazą danych
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Zapytanie SQL
     $stmt = $pdo->query("SELECT lat, lng, description FROM miasta");
     $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Jeśli dane są dostępne, zwróć je w formacie JSON
-    if ($data) {
-        echo json_encode($data);
-    } else {
-        echo json_encode([]); // Jeśli brak danych, zwróć pustą tablicę
-    }
+    echo json_encode($data);
 } catch (PDOException $e) {
-    echo json_encode(["error" => "Błąd połączenia z bazą danych: " . $e->getMessage()]);
+    echo json_encode(["error" => $e->getMessage()]);
 }
 ?>
