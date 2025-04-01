7ad6c923c5fc963c3e07d0d244c6e3a2155c4603
Umbro9 <lolxdlolfabek@gmail.com>
2025-04-01 11:44:53 +0200

    n

--- php/panel/edit-rs.php
+++ php/panel/edit-rs.php
@@ -3,25 +3,33 @@ require("config.php");
 ?>
 
 <div class="profil-container">
-<center>
-	<table>
-		<tr>
-			<th>Nazwa</th>
-			<th>Miasto</th>
-			<th>Edit</th>
-		</tr>
-		<?php
-		// Query to get the list of restaurants with their associated cities
-		$wynik = mysqli_query($conn, "SELECT * FROM restauracje, miasta WHERE lp = id_miasta");
-		while ($wiersz = mysqli_fetch_array($wynik)) {
-			echo '<tr>';
-			echo '<td>' . $wiersz['nazwa'] . '</td>';
-			echo '<td>' . $wiersz['name'] . '</td>';
-			// Link to edit page with the restaurant ID
-			echo '<td><a href="index.php?strona=panel/edit-rs1&id=' . $wiersz['id'] . '">Edit</a></td>';
-			echo '</tr>';
-		}
-		?>
-	</table>
-</center>
+    <center>
+
+
+            <?php
+            $miastaQuery = mysqli_query($conn, "SELECT * FROM miasta");
+            
+            while ($miasto = mysqli_fetch_array($miastaQuery)) {
+                echo '<div class="title-miasto">' . $miasto['name'] . '</div>';
+                
+                $restauracjeQuery = mysqli_query($conn, "SELECT * FROM restauracje WHERE lp = " . $miasto['id_miasta']);
+                
+                while ($restauracja = mysqli_fetch_array($restauracjeQuery)) {
+
+					$imagePath = 'zdj-atrakcje/' . $restauracja["id"] . '.jpg';
+
+
+					echo '<div class="title-miasto-rozwin miasto-rozwin">';
+					if (file_exists($imagePath)) {
+						echo '<img src="' . $imagePath . '" alt="' . $wiersz["nazwa"] . '">';
+					} else {
+						echo '<img src="zdj_atrakcje/nic.png" alt="Brak zdjęcia">';
+					}
+                    echo '<div class="nazwa">' . $restauracja['nazwa'] . '</div>';
+                    echo '<div class="nazwa-link"><a href="index.php?strona=panel/edit-rs1&id=' . $restauracja['id'] . '">Edit</a></div>';
+					echo '</div>';
+                }
+            }
+            ?>
+    </center>
 </div>
