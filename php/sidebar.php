4bb5f91b8652eb302c50b8efa20afa455fcc8a4e
Okruch165 <oskarjablonski272@gmail.com>
2025-04-01 11:25:48 +0200

    ess

--- php/sidebar.php
+++ php/sidebar.php
@@ -21,32 +21,32 @@ if (isset($_SESSION['username'])) {
     echo '<div class="icon"><img src="icons/user.png" alt="Map"></div>';
 }
 ?>
-    <div class="icon"><img src="icons/cogwheel.png" alt="Settings"></div>
+<div class="icon"><img src="icons/cogwheel.png" alt="Settings"></div>
 
     <div class="extra">
         <a href="index.php?strona=srodek"><div class="extra-icon" id="home"><img src="icons/home.png" alt="Home">Główna</div></a>
-        <div class="extra-icon mapa-button" id="map">
-    <img src="icons/map.png" alt="Map"> Mapa
-</div>
+
+        <div class="extra-icon mapa-button" id="mapModalButton">
+            <img src="icons/map.png" alt="Map"> Mapa
+        </div>
 
 <!-- Modal z mapą -->
 <div id="mapModal" class="modal">
-    <div class="modal-content">
-        <span class="close-button">&times;</span>
-        <div id="map" style="width: 100%; height: 95%;"></div>
-    </div>
-</div>
+            <div class="modal-content">
+                <span class="close-button">&times;</span>
+                <div id="map" style="width: 100%; height: 95%;"></div>
+            </div>
+        </div>
 
-        <a href="#"><div class="extra-icon" id="shop"><img src="icons/shop.png" alt="Map">Sklep</div></a>
+        <a href="#"><div class="extra-icon" id="shop"><img src="icons/shop.png" alt="Shop">Sklep</div></a>
         <?php
-if (isset($_SESSION['username'])) {
-    echo '<a href="index.php?strona=profil"><div class="extra-icon"><img src="zdj' . $zdjecie_profilowe_path . '"class="zdj-profilowe">' . htmlspecialchars($skrocone) . '</div></a>';
-} else {
-    echo '<div class="extra-icon konto" id="account"><img src="icons/user.png" alt="Map">Konto</div>';
-}
-?>
+        if (isset($_SESSION['username'])) {
+            echo '<a href="index.php?strona=profil"><div class="extra-icon"><img src="' . $zdjecie_profilowe_path . '" alt="Profilowe" class="zdj-profilowe">' . htmlspecialchars($skrocone) . '</div></a>';
+        } else {
+            echo '<div class="extra-icon konto" id="account"><img src="zdjecia_profilowe/user.png" alt="User">Konto</div>';
+        }
+        ?>
 
-        <a href="index.php?strona=settings"><div class="extra-icon" id="settings"><img src="icons/cogwheel.png" alt="Map">Ustawienia</div></a>
+        <a href="index.php?strona=settings"><div class="extra-icon" id="settings"><img src="icons/cogwheel.png" alt="Settings">Ustawienia</div></a>
     </div>
-
 </aside>
