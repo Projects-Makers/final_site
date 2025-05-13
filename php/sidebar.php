e4790236d447cdb5e8e9ab53ac778863152b1227
Filip Gnat <gnatf@tlimc.local>
2025-05-13 14:20:47 +0200

    s
    s

--- php/sidebar.php
+++ php/sidebar.php
@@ -11,54 +11,43 @@ if (isset($_SESSION['username'])) {
 ?>
 
 <aside class="sidebar">
-    <div class="icon"><a href="index.php?strona=srodek"><img src="icons/home.png" alt="Home"></a></div>
-    <div class="icon"  class="mapa-button"><img src="icons/map.png" alt="Map" ></div>
+    <div class="icon"><img src="icons/home.png" alt="Home"></div>
+    <div class="icon"><img src="icons/map.png" alt="Map"></div>
     <div class="icon"><img src="icons/shop.png" alt="Shop"></div>
     <?php
 if (isset($_SESSION['username'])) {
     echo '<a href="index.php?strona=profil"><div class="icon"><img src="' . $zdjecie_profilowe_path . '" alt="Profilowe" class="zdj-profilowe">'. '</div></a>';
 } else {
-    echo '<div class="icon konto" ><img src="icons/user.png" alt="User"></div>';
+    echo '<div class="icon"><img src="icons/user.png" alt="Map"></div>';
 }
 ?>
-<div class="icon"><a href="index.php?strona=settings"><img src="icons/cogwheel.png" alt="Settings"></a></div>
+    <div class="icon"><img src="icons/cogwheel.png" alt="Settings"></div>
 
     <div class="extra">
-        <a href="index.php?strona=srodek"><div class="extra-icon" id="home"><img src="icons/home.png" alt="Home">Główna</div></a>
-
-        <div class="extra-icon mapa-button" id="mapModalButton">
-            <img src="icons/map.png" alt="Map"> Mapa
-        </div>
+        <a href="index.php?strona=srodek"><div class="extra-icon"><img src="icons/home.png" alt="Home">Główna</div></a>
+        <div class="extra-icon mapa-button">
+    <img src="icons/map.png" alt="Map"> Mapa
+</div>
 
 <!-- Modal z mapą -->
 <div id="mapModal" class="modal">
-            <div class="modal-content">
-                <span class="close-button">&times;</span>
-                <div id="map" style="width: 100%; height: 95%;"></div>
-            </div>
-        </div>
-	<script>
-	<style>
-        button {
-            all: unset; 
-            cursor: pointer; 
-        }
-	</script>
-    <style>
-        button {
-            all: unset;
-            cursor: pointer; 
-        }
-	</style>
-        <button onclick="trip()"><div class="extra-icon" id="shop"><img src="icons/shop.png" alt="Shop">Sklep</div></button>
+    <div class="modal-content">
+        <span class="close-button">&times;</span>
+        <div id="map" style="width: 100%; height: 95%;"></div>
+    </div>
+</div>
+
+        <a href="#"><div class="extra-icon"><img src="icons/shop.png" alt="Map">Sklep</div></a>
+<div class="sklep">nigger</div>
         <?php
 if (isset($_SESSION['username'])) {
-    echo '<a href="index.php?strona=profil"><div class="extra-icon"><img src="' . $zdjecie_profilowe_path . '" alt="Profilowe" class="zdj-profilowe">' . htmlspecialchars($skrocone) . '</div></a>';
+    echo '<a href="index.php?strona=profil"><div class="extra-icon"><img src="' . $zdjecie_profilowe_path . '"class="zdj-profilowe">' . htmlspecialchars($skrocone) . '</div></a>';
 } else {
-    echo '<div class="extra-icon konto"><img src="icons/user.png" alt="User">Konto</div></a>';
+    echo '<div class="extra-icon konto"><img src="icons/user.png" alt="Map">Konto</div>';
 }
 ?>
 
-        <a href="index.php?strona=settings"><div class="extra-icon" id="settings"><img src="icons/cogwheel.png" alt="Settings">Ustawienia</div></a>
+        <a href="index.php?strona=settings"><div class="extra-icon"><img src="icons/cogwheel.png" alt="Map">Ustawienia</div></a>
     </div>
+
 </aside>
